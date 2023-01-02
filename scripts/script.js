a42ef890e3cf2c6d67550a3701619da0a2cc2fb1
Dmitry V <pub.dv@ya.ru>
2023-01-02 19:33:13 +0300

    feat:media queries done

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,8 @@ let btnEditOpen = document.querySelector('.profile__btn-edit');
 let btnEditClose = document.querySelector('.popup__btn-close');
 let profileEditPopup = document.querySelector('.popup');
 let saveForm = document.querySelector('.popup__form');
-let nameInput = document.querySelector('#popup__input_type_name');
-let aboutInput = document.querySelector('#popup__input_type_about');
+let nameInput = document.querySelector('#input_type_name');
+let aboutInput = document.querySelector('#input_type_about');
 let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about');
 
