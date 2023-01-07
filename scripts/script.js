88d22299babc22af941ae089ea5ca11ef9e3894c
Dmitry V <pub.dv@ya.ru>
2023-01-07 14:04:19 +0300

    fix: fix all issues according last review

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,26 @@
-let btnEditOpen = document.querySelector('.profile__btn-edit');
-let btnEditClose = document.querySelector('.popup__btn-close');
-let profileEditPopup = document.querySelector('.popup');
-let saveForm = document.querySelector('.popup__form');
-let nameInput = document.querySelector('#input_type_name');
-let aboutInput = document.querySelector('#input_type_about');
-let profileName = document.querySelector('.profile__name');
-let profileAbout = document.querySelector('.profile__about');
+let profile__btn_edit = document.querySelector('.profile__btn-edit');
+let popup__btn_close = document.querySelector('.popup__btn-close');
+let popup_type_edit_profile = document.querySelector('.popup_type_edit-profile');
+let profile_edit_form = document.getElementsByName('profile_edit_form')[0];
+let popup__input_el_name = document.querySelector('.popup__input_el_name');
+let popup__input_el_about = document.querySelector('.popup__input_el_about');
+let profile__name = document.querySelector('.profile__name');
+let profile__about = document.querySelector('.profile__about');
 
-function toggleProfileEditPopup (evt) {
+function toggle_popup_type_edit_profile (evt) {
   evt.preventDefault();
-  profileEditPopup.classList.toggle('popup_opened');
+  popup__input_el_name.value = profile__name.textContent;
+  popup__input_el_about.value = profile__about.textContent;
+  popup_type_edit_profile.classList.toggle('popup_opened');
 }
 
-function handleFormSubmit (evt) {
+function handle_profile_edit_form_submit (evt) {
   evt.preventDefault();
-  profileName.textContent = nameInput.value;
-  profileAbout.textContent = aboutInput.value;
+  profile__name.textContent = popup__input_el_name.value;
+  profile__about.textContent = popup__input_el_about.value;
+  popup_type_edit_profile.classList.toggle('popup_opened');
 }
 
-btnEditOpen.addEventListener('click', toggleProfileEditPopup);
-btnEditClose.addEventListener('click', toggleProfileEditPopup);
-saveForm.addEventListener('submit', handleFormSubmit);
+profile__btn_edit.addEventListener('click', toggle_popup_type_edit_profile);
+popup__btn_close.addEventListener('click', toggle_popup_type_edit_profile);
+profile_edit_form.addEventListener('submit', handle_profile_edit_form_submit);
