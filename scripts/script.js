4787e5242d33094b23cdfa45e2f00cad3623a9b0
Dmitry V <pub.dv@ya.ru>
2023-01-07 17:34:14 +0300

    fix: variables names in camelCase done

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,26 @@
-let profile__btn_edit = document.querySelector('.profile__btn-edit');
-let popup__btn_close = document.querySelector('.popup__btn-close');
-let popup_type_edit_profile = document.querySelector('.popup_type_edit-profile');
-let profile_edit_form = document.getElementsByName('profile_edit_form')[0];
-let popup__input_el_name = document.querySelector('.popup__input_el_name');
-let popup__input_el_about = document.querySelector('.popup__input_el_about');
-let profile__name = document.querySelector('.profile__name');
-let profile__about = document.querySelector('.profile__about');
+let profileBtnEdit = document.querySelector('.profile__btn-edit');
+let popupBtnClose = document.querySelector('.popup__btn-close');
+let popupTypeEditProfile = document.querySelector('.popup_type_edit-profile');
+let profileEditForm = document.getElementsByName('profile_edit_form')[0];
+let popupInputElName = document.querySelector('.popup__input_el_name');
+let popupInputElAbout = document.querySelector('.popup__input_el_about');
+let profileName = document.querySelector('.profile__name');
+let profileAbout = document.querySelector('.profile__about');
 
-function toggle_popup_type_edit_profile (evt) {
+function togglePopupTypeEditProfile (evt) {
   evt.preventDefault();
-  popup__input_el_name.value = profile__name.textContent;
-  popup__input_el_about.value = profile__about.textContent;
-  popup_type_edit_profile.classList.toggle('popup_opened');
+  popupInputElName.value = profileName.textContent;
+  popupInputElAbout.value = profileAbout.textContent;
+  popupTypeEditProfile.classList.toggle('popup_opened');
 }
 
-function handle_profile_edit_form_submit (evt) {
+function handleProfileEditFormSubmit (evt) {
   evt.preventDefault();
-  profile__name.textContent = popup__input_el_name.value;
-  profile__about.textContent = popup__input_el_about.value;
-  popup_type_edit_profile.classList.toggle('popup_opened');
+  profileName.textContent = popupInputElName.value;
+  profileAbout.textContent = popupInputElAbout.value;
+  popupTypeEditProfile.classList.toggle('popup_opened');
 }
 
-profile__btn_edit.addEventListener('click', toggle_popup_type_edit_profile);
-popup__btn_close.addEventListener('click', toggle_popup_type_edit_profile);
-profile_edit_form.addEventListener('submit', handle_profile_edit_form_submit);
+profileBtnEdit.addEventListener('click', togglePopupTypeEditProfile);
+popupBtnClose.addEventListener('click', togglePopupTypeEditProfile);
+profileEditForm.addEventListener('submit', handleProfileEditFormSubmit);
