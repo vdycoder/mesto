1f3b2bdc10868bd5f17e64a118c6dcbf34e9446e
Dmitry V <pub.dv@ya.ru>
2023-02-07 14:24:57 +0300

    feat: add card popup done

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,25 @@
 let profileBtnEdit = document.querySelector('.profile__btn-edit');
-let popupBtnClose = document.querySelector('.popup__btn-close');
+let profileBtnAdd = document.querySelector('.profile__btn-add');
+
+let profileBtnClose = document.querySelectorAll('.popup__btn-close')[0];
+let addCardBtnClose = document.querySelectorAll('.popup__btn-close')[1];
+
 let popupTypeEditProfile = document.querySelector('.popup_type_edit-profile');
+
+let popupTypeAddCard = document.querySelector('.popup_type_add-card');
+
 let profileEditForm = document.getElementsByName('profile_edit_form')[0];
+let addCardForm = document.getElementsByName('profile_edit_form')[1];
+
 let popupInputElName = document.querySelector('.popup__input_el_name');
 let popupInputElAbout = document.querySelector('.popup__input_el_about');
+
+let popupInputElCaption = document.querySelector('.popup__input_el_caption');
+let popupInputElImage = document.querySelector('.popup__input_el_image');
+
 let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about');
 
-function togglePopupTypeEditProfile (evt) {
-  evt.preventDefault();
-  popupInputElName.value = profileName.textContent;
-  popupInputElAbout.value = profileAbout.textContent;
-  popupTypeEditProfile.classList.toggle('popup_opened');
-}
-
-function handleProfileEditFormSubmit (evt) {
-  evt.preventDefault();
-  profileName.textContent = popupInputElName.value;
-  profileAbout.textContent = popupInputElAbout.value;
-  popupTypeEditProfile.classList.toggle('popup_opened');
-}
-
-profileBtnEdit.addEventListener('click', togglePopupTypeEditProfile);
-popupBtnClose.addEventListener('click', togglePopupTypeEditProfile);
-profileEditForm.addEventListener('submit', handleProfileEditFormSubmit);
-
-/*sprint 5*/
 const initialCards = [
   {
     name: 'Архыз',
@@ -55,6 +49,50 @@ const initialCards = [
 
 const elementTemplate = document.querySelector('#element_template').content;
 const elementsList = document.querySelector('.elements__list');
+
+
+function togglePopupTypeEditProfile (evt) {
+  evt.preventDefault();
+  popupInputElName.value = profileName.textContent;
+  popupInputElAbout.value = profileAbout.textContent;
+  popupTypeEditProfile.classList.toggle('popup_opened');
+}
+
+function handleProfileEditFormSubmit (evt) {
+  evt.preventDefault();
+  profileName.textContent = popupInputElName.value;
+  profileAbout.textContent = popupInputElAbout.value;
+  popupTypeEditProfile.classList.toggle('popup_opened');
+}
+
+function togglePopupTypeAddCard (evt) {
+  evt.preventDefault();
+  popupTypeAddCard.classList.toggle('popup_opened');
+}
+
+function handleAddCardFormSubmit (evt) {
+  evt.preventDefault();
+  const singleElement = elementTemplate.querySelector('.elements__item').cloneNode(true);
+
+  // наполняем содержимым
+  singleElement.querySelector('.elements__image').src = popupInputElImage.value;
+  singleElement.querySelector('.elements__image').alt = popupInputElCaption.value;
+  singleElement.querySelector('.elements__caption').textContent = popupInputElCaption.value;
+
+  // отображаем на странице
+  elementsList.prepend(singleElement);
+  popupTypeAddCard.classList.toggle('popup_opened');
+}
+
+profileBtnEdit.addEventListener('click', togglePopupTypeEditProfile);
+profileBtnClose.addEventListener('click', togglePopupTypeEditProfile);
+profileEditForm.addEventListener('submit', handleProfileEditFormSubmit);
+addCardForm.addEventListener('submit', handleAddCardFormSubmit);
+profileBtnAdd.addEventListener('click', togglePopupTypeAddCard);
+addCardBtnClose.addEventListener('click', togglePopupTypeAddCard);
+
+
+/*sprint 5*/
 
 initialCards.forEach(((item) => {
   // клонируем содержимое тега template
